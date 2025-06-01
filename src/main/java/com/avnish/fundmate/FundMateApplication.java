1b8c161e5513bca98d5755d3b408c07b49af740a
AVNISH KUMAR <avnishkrsharma3@gmail.com>
2025-06-01 09:22:03 +0530

    Project Setup

---
+++ src/main/java/com/avnish/fundmate/FundMateApplication.java
@@ -0,0 +1,13 @@
+package com.avnish.fundmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avnish/fundmate/FundMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avnish.fundmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
